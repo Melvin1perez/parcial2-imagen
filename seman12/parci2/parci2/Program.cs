03a576621b5611c1d71fe4df6474ea5dcd195390
Melvin1perez <125324725+Melvin1perez@users.noreply.github.com>
2023-04-18 20:56:38 -0600

    Add files via upload

---
+++ seman12/parci2/parci2/Program.cs
@@ -0,0 +1,17 @@
+﻿//examen parcial 2 melvin perez
+
+int dato1 = 16;
+string dato2 = "25g";
+
+try
+{
+    int resultado = dato1 + Convert.ToInt32(dato2);
+    Console.WriteLine(resultado);
+
+}
+catch (Exception)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
